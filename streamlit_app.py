982ac7d82d5c24f354f8a99bfa64edaafec19f11
Dharma Toolman <melpame@users.noreply.github.com>
2024-10-15 07:56:34 +0000

    username without masking

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.title("Secure login Page Example")
 
 # 두 가지 로그인 페이지 선택
-login_option = st.selectbox("Choose Login Page:", ["Login Page 1", "Login Page 2"])
+login_option = st.selectbox("Choose Login Page:", ["Login Page 1", "Login Page 2 with masking"])
 
 # Login Page 1: 표준 비밀번호 필드 사용 (type="password")
 if login_option == "Login Page 1":
@@ -15,7 +15,7 @@ if login_option == "Login Page 1":
         st.write(f"Login 1 Submitted! Username: {username}, Password: {password}")
 
 # Login Page 2: type="text"로 마스킹된 비밀번호 필드 사용
-elif login_option == "Login Page 2":
+elif login_option == "Login Page 2 with masking":
     st.subheader("Login with Text Field and Masking")
     username = st.text_input("Username")
     password = st.text_input("Password (Masked as Text)", type="default")  # 일반 텍스트 필드
